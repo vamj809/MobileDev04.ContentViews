a2bf350cbc525de8d467e125da8c6862bfd53c89
Víctor A. Montero <vamj809@users.noreply.github.com>
2021-09-10 04:16:19 -0400

    removed unused code

--- MobileDev04.ContentViews/MobileDev04.ContentViews/ViewModels/MainMenuViewModel.cs
+++ MobileDev04.ContentViews/MobileDev04.ContentViews/ViewModels/MainMenuViewModel.cs
@@ -7,12 +7,6 @@ namespace MobileDev04.ContentViews.ViewModels
     {
         public MenuOption SelectedMenuOption { get; set; }
         public ObservableCollection<MenuOption> DiscoveryMenuOptions { get; }
-        public MenuOption BarsMenuOption { get; }
-        public MenuOption DinersMenuOption { get; }
-        public MenuOption CafesMenuOption { get; }
-        public MenuOption NearbyMenuOption { get; }
-        public MenuOption FastFoodsMenuOption { get; }
-        public MenuOption FeaturedFoodsMenuOption { get; }
         public MainMenuViewModel() {
             DiscoveryMenuOptions = new ObservableCollection<MenuOption>() {
                 new MenuOption("BarIcon", "Bars & Hotels", "42 Places"),
@@ -22,12 +16,6 @@ namespace MobileDev04.ContentViews.ViewModels
                 new MenuOption("FastFoodIcon", "Fast Foods", "29 Places"),
                 new MenuOption("PizzaIcon", "Featured Foods", "21 Places")
             };
-            BarsMenuOption = new MenuOption("BarIcon", "Bars & Hotels", "42 Places");
-            DinersMenuOption = new MenuOption("DinerIcon", "Fine Dining", "15 Places");
-            CafesMenuOption = new MenuOption("CafeIcon", "Cafes", "28 Places");
-            NearbyMenuOption = new MenuOption("NearbyIcon", "Nearby", "34 Places");
-            FastFoodsMenuOption = new MenuOption("FastFoodIcon", "Fast Foods", "29 Places");
-            FeaturedFoodsMenuOption = new MenuOption("PizzaIcon", "Featured Foods", "21 Places");
         }
     }
 }
